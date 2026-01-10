5d836243e167c7210b779593e2dc2b4af2d0ac22
True_Toamim <ferreiraalves182020@outlook.com>
2026-01-10 20:20:01 -0300

    hello

--- EX005.lua
+++ EX005.lua
@@ -1,6 +1,64 @@
-local ainthere = 1
+local contagem_low_roll = 0
+local contagem_high_roll = 0
 
-while ainthere <= 20 do
+local total_de_simulacoes = 100000000000
+
+print("Comecando a simulacao de " .. total_de_simulacoes .. " rolagem de dados.")
+
+for i = 1, total_de_simulacoes do
+    
+    local roll = math.random(1, 6)
+
+    if roll <= 3 then
+
+        contagem_low_roll = contagem_low_roll + 1
+
+        else
+
+            contagem_high_roll = contagem_high_roll + 1
+
+    end
+
+    if i == 25000000000 or i == 50000000000 then
+
+        print(i)
+        
+    end
+
+end
+
+print("A quantidade de Low rolls foi: " .. contagem_low_roll)
+print("A quantidade de High rolls foi: " .. contagem_high_roll)
+
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+--[[
+
+for loop1 = 1, 100000 do
+
+    print("Nos estamos no loop numero " .. loop1 .. ":")
+
+    local roll = math.random(1, 6)
+
+    if roll <= 3 then
+
+        print("O seu dado caiu em: " .. roll .. ". Um Low Roll!")
+
+        else
+
+            print("O seu dado caiu em: " .. roll .. ". Um High Roll!")
+        
+    end
+    
+end
+
+]]
+
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+--[[
+
+while ainthere ~= 21 do
 
     ainthere = ainthere + 1
     local ainthere2 = math.random(1, 6)
@@ -16,3 +74,5 @@ while ainthere <= 20 do
     end
 
 end
+
+]]
