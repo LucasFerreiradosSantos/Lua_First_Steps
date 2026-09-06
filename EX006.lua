66c22bf148181f208e18da30177d2a74ee6ecad0
True_Toamim <ferreiraalves182020@outlook.com>
2026-09-06 20:00:29 -0300

    aoaoa

---
+++ EX006.lua
@@ -0,0 +1,16 @@
+local playerinput = 20
+local probabilidade = playerinput / 100
+print(probabilidade)
+
+local playerinput1 = 12
+local probabilidade1 = playerinput1 / 100
+print(probabilidade1)
+
+local playerinput2 = 6
+local probabilidade2 = playerinput2 / 100
+print(probabilidade2)
+
+
+
+local probabilidade3 = probabilidade * probabilidade1 * probabilidade2
+print(probabilidade3)
